a708f91878815f7d7bc1e52f570be881ac702cd6
Dmitrii L <hexletdmitrii@gmail.com>
2024-05-18 01:25:38 +0300

    2-sd task

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,4 +4,6 @@ import argparse
 parser = argparse.ArgumentParser(description = 'Compares two configuration files and shows a difference.')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format', help = 'set format of output')
+
 print(parser.parse_args())
