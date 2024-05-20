b5c5f3b4e8f1f8afea9ed06c737caadab88b2757
Dmitrii L <hexletdmitrii@gmail.com>
2024-05-20 22:50:08 +0300

    correcter

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from pathlib import Path
 
 
 def main():
@@ -10,8 +11,12 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    file1 = json.load(open(args.first_file))
-    file2 = json.load(open(args.second_file))
+    script_dir = Path(__file__).parent
+    script_dir = script_dir.parent
+    file_path1 = script_dir / 'tests' / 'fixtures' / args.first_file
+    file_path2 = script_dir / 'tests' / 'fixtures' / args.second_file
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
     print(generate_diff(file1, file2))
 
 
@@ -22,9 +27,15 @@ def generate_diff(file1, file2):
     for i in sorted(set1 | set2):
         if file1.get(i) == file2.get(i):
             result = result + '\n    ' + i + ': ' + str(file1[i]).lower()
-        if i in set1 - set2 or i in set1 and i in set2 and file1.get(i) != file2.get(i):
+        if (
+            i in set1 - set2 or
+            (i in set1 and i in set2 and file1.get(i) != file2.get(i))
+        ):
             result = result + '\n  - ' + i + ': ' + str(file1[i]).lower()
-        if i in set2 - set1 or i in set1 and i in set2 and file1.get(i) != file2.get(i):
+        if (
+            i in set2 - set1 or
+            (i in set1 and i in set2 and file1.get(i) != file2.get(i))
+        ):
             result = result + '\n  + ' + i + ': ' + str(file2[i]).lower()
     result = result + '\n}'
     return result
--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,14 +1,16 @@
 from gendiff.scripts.gendiff import generate_diff
 import json
+from pathlib import Path
 
 
 def test_generate_diff():
-    file1 = json.load(open('/home/dmitrii/hexlet/python-project-50/gendiff/tests/fixtures/file1.json'))
-    file2 = json.load(open('/home/dmitrii/hexlet/python-project-50/gendiff/tests/fixtures/file2.json'))
-    f = open('/home/dmitrii/hexlet/python-project-50/gendiff/tests/fixtures/right_test_gendiff.txt')
+    script_dir = Path(__file__).parent
+    file_path1 = script_dir / 'fixtures' / 'file1.json'
+    file_path2 = script_dir / 'fixtures' / 'file2.json'
+    correct_result_path = script_dir / 'fixtures' / 'right_test_gendiff.txt'
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+    f = open(correct_result_path)
     text = f.read()
     f.close
     assert generate_diff(file1, file2) == text
-    
- 
-test_generate_diff()
