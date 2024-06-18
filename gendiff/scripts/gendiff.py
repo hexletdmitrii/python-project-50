70bd9bf257308a65a59469394810d4eff36a8a9c
Dmitrii L <hexletdmitrii@gmail.com>
2024-06-18 17:50:14 +0300

    update1

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -33,8 +33,11 @@ def generate_diff(file1, file2, format='stylish'):
 
 
 def parser_file(file_name):
-    script_dir = Path(__file__).parent.parent
-    file_path = script_dir / 'tests' / 'fixtures' / file_name
+    if '/' in file_name:
+        file_path = file_name
+    else:
+        script_dir = Path(__file__).parent.parent
+        file_path = script_dir / 'tests' / 'fixtures' / file_name
     if file_name.endswith('json'):
         with open(file_path) as f:
             return json.load(f)
