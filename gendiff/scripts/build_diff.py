01f42a0187032bdb767eaf59ad69897e6a1c276d
Dmitrii L <hexletdmitrii@gmail.com>
2024-06-22 04:55:42 +0300

    test5

--- gendiff/scripts/build_diff.py
+++ gendiff/scripts/build_diff.py
@@ -1,23 +1,25 @@
 def build_diff(data1, data2):
     result = []
+
     if not isinstance(data1, dict):
         data1 = {}
     if not isinstance(data2, dict):
         data2 = {}
+
     for key in sorted(set(data1.keys()) | set(data2.keys())):
         value1 = data1.get(key)
         value2 = data2.get(key)
-        if key not in data1.keys():
+        if key not in data1:
             result.append({
                 'key': key,
                 'action_type': 'added',
-                'value': data2.get(key)
+                'value': value2
             })
-        elif key not in data2.keys():
+        elif key not in data2:
             result.append({
                 'key': key,
                 'action_type': 'removed',
-                'value': data1.get(key)
+                'value': value1
             })
         elif isinstance(value1, dict) and isinstance(value2, dict):
             result.append({
